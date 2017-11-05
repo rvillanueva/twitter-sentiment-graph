e0ccd2b9ed2af1435abb09d7983c296be3bfacf5
Ryan Villanueva <ryanvill@gmail.com>
2017-11-05 14:41:52 -0500

    Swap color and radius significance

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,20 +13,24 @@ var messages = [];
 
 var i = 0;
 var startTime = 2000;
-var endTime = 200;
-
+var endTime = 400;
+var steps = 15;
+var currentSpeed = startTime;
 addSeedNode();
 addNewNodes();
 
 function addNewNodes(){
   setTimeout(() => {
     if(i < graph.steps.length){
+      if(currentSpeed > endTime){
+        currentSpeed -= (startTime - endTime)/steps;
+      }
       console.log(displayed)
       addStep(graph.steps[i]);
       i++;
       addNewNodes();
     }
-  }, (startTime - endTime) * (graph.steps.length - i) / graph.steps.length + endTime);
+  }, currentSpeed);
 }
 
 function addSeedNode(){
@@ -102,19 +106,22 @@ function getMasterNode(id){
 
 
 
-var svg = d3.select("svg"),
-    width = +svg.attr("width"),
+var svg = d3.select("svg");
+svg.attr('width', window.innerWidth - 300);
+svg.attr('height', window.innerHeight);
+
+    var width = +svg.attr("width"),
     height = +svg.attr("height"),
     color = d3.scaleOrdinal(d3.schemeCategory10);
 
 var simulation = d3.forceSimulation(displayed.nodes)
-    .force("charge", d3.forceManyBody().strength(-500))
-    .force("link", d3.forceLink(displayed.links).distance(90))
+    .force("charge", d3.forceManyBody().strength(-350))
+    .force("link", d3.forceLink(displayed.links).distance(75))
     .force("x", d3.forceX())
     .force("y", d3.forceY())
     .alphaTarget(1)
     .on("tick", ticked);
-var g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")"),
+var g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")").attr('id', 'graph-container'),
     link = g.append("g").attr("stroke", "#000").attr("stroke-width", 1).selectAll(".link"),
     node = g.append("g").selectAll(".node");
 restart();
@@ -122,8 +129,10 @@ restart();
 function restart() {
   displayed.nodes = displayed.nodes.map(node => {
     node.radius = getRadiusScale(node);
+    node.edgesScore = getEdgesScore(node);
     return node;
   })
+
   // Apply the general update pattern to the nodes.
   node = node.data(displayed.nodes, function(d) { return d.id;});
   node.exit().remove();
@@ -142,6 +151,7 @@ function restart() {
       .attr("ry", 24);         // set the y radius
   holder // give the clipPath an ID
     .append("ellipse")          // shape it as an ellipse
+    .attr("id", "color-stroke")
       .attr("cx", 0)         // position the x-centre
       .attr("cy", 0)         // position the y-centre
       .attr("rx", 24)         // set the x radius
@@ -158,6 +168,7 @@ function restart() {
         .attr("y", -24)
   holder // give the clipPath an ID
     .append("ellipse")          // shape it as an ellipse
+    .attr("id", "color-overlay") // give the clipPath an ID
       .attr("cx", 0)         // position the x-centre
       .attr("cy", 0)         // position the y-centre
       .attr("rx", 24)         // set the x radius
@@ -177,9 +188,25 @@ function restart() {
   simulation.force("link").links(displayed.links);
   simulation.alpha(1).restart();
   renderTweetBox();
+
+
+  var bounds = d3.select("#graph-container").node().getBBox();
+
+    var scale = Math.min(1.2/Math.max(bounds.width/width, bounds.height/height), 1),
+    translate = [-bounds.x * scale + (width - bounds.width * scale) / 2, -bounds.y * scale + (height - bounds.height * scale)/2];
+
+g.transition()
+    .duration(750)
+    .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
+
 }
 function ticked() {
   node.attr("transform", function(d) { return `translate(${d.x},${d.y}) scale(${d.radius})`; })
+      .selectAll("#color-overlay")
+        .attr("fill", function(d) { return getColor(d)})
+        .attr("opacity", function(d) { return getOpacity(d, 0, 0.5)})
+      .selectAll("#color-stroke")
+        .attr("stroke", function(d) {return getColor(d)})
       //.attr("x", function(d) { return d.x - 20; })
       //.attr("y", function(d) { return d.y - 20; })
   link.attr("x1", function(d) { return d.source.x; })
@@ -189,10 +216,10 @@ function ticked() {
 }
 
 function getColor(node){
-  if(typeof node.score === 'number'){
-    var normalizedScore = normalizeScore(node.score);
+  if(typeof node.edgesScore === 'number'){
+    var normalizedScore = normalizeEdgesScore(node.edgesScore);
     var hue;
-    if(node.score > 0){
+    if(node.edgesScore > 0){
       hue = 240
     } else {
       hue = 0;
@@ -205,7 +232,7 @@ function getColor(node){
 
 function getOpacity(node, min, max){
   if(typeof node.score === 'number'){
-    var normalizedScore = (normalizeScore(node.score) - 0.5) * 2;
+    var normalizedScore = (normalizeEdgesScore(node.edgesScore) - 0.5) * 2;
     return Math.abs(normalizedScore) * (max - min) + min;
   }
   return 0;
@@ -216,21 +243,23 @@ function getLinkStroke(node){
 }
 
 function getRadiusScale(node){
-  var minScale = 0.8;
-  var mentionScoreTotal = 0;
-  var mentionCount = 0;
-  var mentionScore = 0;
+  var minScale = 0.3;
+  var scale = minScale + normalizeScore(node.score) * 1.5;
+  return scale;
+}
+
+function getEdgesScore(node){
+  var scoreTotal = 0;
+  var count = 0;
   displayed.links.map(link => {
     if((link.source.id === node.id || link.target.id === node.id) && typeof link.score === 'number'){
-      mentionScoreTotal += link.score;
-      mentionCount ++;
+      scoreTotal += link.score;
+      count ++;
     }
   })
-  if(mentionCount > 1){
-    mentionScore = mentionScoreTotal/mentionCount;
-  }
-  var scale = minScale + Math.min(normalizeMentionScore(mentionScore) * 2, 2);
-  return scale;
+  scoreTotal += node.score;
+  count++;
+  return scoreTotal/count;
 }
 
 function normalizeScore(score){
@@ -238,7 +267,7 @@ function normalizeScore(score){
   return Math.max(Math.min(normalized, 1), 0);
 }
 
-function normalizeMentionScore(score){
+function normalizeEdgesScore(score){
   var normalized = score / graph.maxEdgesScore;
   return Math.max(Math.min(normalized, 1), 0);
 }
